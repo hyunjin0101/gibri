875f1b57ed8f4fc5b3f479397be19c9beac4b855
hyunjin0101 <hjin0321@naver.com>
2024-11-30 15:21:01 +0900

    포스터

--- e.js
+++ e.js
@@ -1,3 +1,12 @@
+const posterLinks = [
+  'https://tngodj.github.io/Totoro/', // '1.jpg'에 연결된 링크
+  'https://tngodj.github.io/Cat/', // '7.jpg'에 연결된 링크
+  'https://tngodj.github.io/Haul/', // '3.jpeg'에 연결된 링크
+  'https://tngodj.github.io/Sen-Chihiro/', // '4.jpg'에 연결된 링크
+  'https://tngodj.github.io/Ponyo/',  // '5.jpg'에 연결된 링크
+  'https://tngodj.github.io/Laputa/' // '6.jpg'에 연결된 링크
+];
+
 const colors = ["#ff7f50", "#87ceeb", "#98fb98", "#ff69b4", "#dda0dd"];
 const posteres = document.querySelectorAll('.poster');
 const coverflow = document.querySelector('.coverflow');
@@ -14,6 +23,18 @@ function updateCoverflow() {
       poster.classList.add('active');
     }
   });
+
+  // 포스터에 클릭 이벤트 추가
+posteres.forEach((poster, index) => {
+  poster.addEventListener('click', () => {
+    if (poster.classList.contains('active')) { // 활성화된 포스터만 클릭 가능
+      const link = posterLinks[index]; // 해당 포스터의 링크 가져오기
+      if (link) {
+        window.open(link, '_blank'); // 새 탭에서 링크 열기
+      }
+    }
+  });
+});
 
   // 포스터 이동
   const offset = -(activeIndex * 220); // 각 포스터의 넓이 + 간격
